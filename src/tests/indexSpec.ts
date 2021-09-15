804a7dbbecfe5b2a7d0c4cec5c301fed08b1f1b9
Danyyl <danyyl.con@gmail.com>
2021-09-15 00:21:04 -0400

    create the spec and the functions for 2 specs, configure tsconfig

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -10,7 +10,12 @@ it("should get basic data on the country canada", async () => {
 });
 
 /** Add test for getRegionCountries function here */
-
+it("should get the countries in the region NAFTA", async () => {
+    const data = await countries.getRegionCountries('nafta');
+    expect(data).toEqual([
+        'Canada', 'Mexico', 'United States of America'
+    ]);
+});
 
 it("should get capitals of NAFTA countries", async () => {
   const data = await countries.getRegionCapitals('nafta');
